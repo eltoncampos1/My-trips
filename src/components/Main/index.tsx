6d27853bd9c66b329c3c4a215e60732dc302b3bd
elton de campos <eltoncampos36@gmail.com>
2021-03-07 01:07:42 -0300

    chore: start setup project

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,19 +1,5 @@
-import * as S from './styles';
-
 const Main = () => {
-  return (
-    <S.Wrapper>
-      <S.Logo src="/img/logo.svg" alt="React avançado logo" />
-      <S.Title>React avançado</S.Title>
-      <S.Description>
-        Typescript, ReactJS, NextJS and styled components
-      </S.Description>
-      <S.Illustration
-        src="/img/hero-illustration.svg"
-        alt="desenvolvedor em frente ao computador"
-      />
-    </S.Wrapper>
-  );
+  return <h1>Next Js</h1>;
 };
 
 export default Main;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Boilerplate - NextApp</title>
+        <title>My trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
         <meta
           name="description"
-          content="A simple boilerplate to work with TS react, nextJS and styled-components"
+          content="A simple project to show my favorite spots in the world"
         />
       </Head>
       <GlobalStyles />
