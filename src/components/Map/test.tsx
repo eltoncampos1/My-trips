24ff910e04f841a835aa3faa3c49378c05acf4e1
elton de campos <eltoncampos36@gmail.com>
2021-03-07 02:46:34 -0300

    test: Should render with the marker in correct place

--- src/components/Map/test.tsx
+++ src/components/Map/test.tsx
@@ -9,5 +9,30 @@ describe('<Map />', () => {
       screen.getByRole('link', { name: /a js library for interactive maps/i })
     ).toBeInTheDocument();
   });
-  // it('Should render with the marker in correct place', () => {});
+  it('Should render with the marker in correct place', () => {
+    const place = {
+      id: '1',
+      name: 'São Paulo',
+      slug: 'São Paulo',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    };
+
+    const placetwo = {
+      id: '2',
+      name: 'Rio de Janeiro',
+      slug: 'Rio de Janeiro',
+      location: {
+        latitude: 1,
+        longitude: 1
+      }
+    };
+
+    render(<Map places={[place, placetwo]} />);
+
+    expect(screen.getByTitle(/São Paulo/i)).toBeInTheDocument();
+    expect(screen.getByTitle(/Rio de Janeiro/i)).toBeInTheDocument();
+  });
 });
