5046439f4c9cb322fe353986e2e81d68e5ed9a1b
elton de campos <eltoncampos36@gmail.com>
2021-03-08 01:01:58 -0300

    chore: add p tag to body

--- src/templates/About/index.tsx
+++ src/templates/About/index.tsx
@@ -12,10 +12,12 @@ const AboutTemplate = () => (
     <S.Heading>My trips</S.Heading>
 
     <S.Body>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, aperiam
-      praesentium. Error, excepturi? Officia laudantium unde voluptatem nesciunt
-      provident dicta repellat porro repudiandae odio iste vero vitae, dolores
-      eligendi commodi?
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, aperiam
+        praesentium. Error, excepturi? Officia laudantium unde voluptatem
+        nesciunt provident dicta repellat porro repudiandae odio iste vero
+        vitae, dolores eligendi commodi?
+      </p>
     </S.Body>
   </S.Content>
 );
